6da66875d8809ca0926519f7e132a9f80c9d1eba
rocky-chowdhury-api <rvai27920@gmail.com>
2026-03-25 02:03:52 +0600

    Update song.js

--- api/song.js
+++ api/song.js
@@ -1,18 +1,35 @@
-const axios = require("axios");
+const yts = require("yt-search");
+const ytdl = require("@distube/ytdl-core");
 
-export default async function handler(req, res) {
-  const { query } = req.query;
+module.exports = async (req, res) => {
+  try {
+    const query = req.query.query;
 
-  if (!query) {
-    return res.status(400).json({
-      status: false,
-      message: "Query missing"
+    if (!query) {
+      return res.status(400).json({
+        error: "query lagbe"
+      });
+    }
+
+    const search = await yts(query);
+    const video = search.videos[0];
+
+    if (!video) {
+      return res.status(404).json({
+        error: "song pawa jai nai"
+      });
+    }
+
+    const stream = ytdl(video.url, {
+      filter: "audioonly"
+    });
+
+    res.setHeader("Content-Type", "audio/mpeg");
+    stream.pipe(res);
+
+  } catch (e) {
+    res.status(500).json({
+      error: e.message
     });
   }
-
-  return res.status(200).json({
-    status: true,
-    message: "API working",
-    query: query
-  });
-}
+};
